6e67eedd89ab3d0ab493913d8ac379ad2c3b46c6
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-12-19 11:48:20 +0100

    directions -> <- up dow

--- my_lib/sl_utils/directions.c
+++ my_lib/sl_utils/directions.c
@@ -6,7 +6,7 @@
 /*   By: ahel-mou <ahel-mou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 16:40:55 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/18 16:41:39 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/18 18:10:22 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,11 @@ void	up(t_unit *vr3)
 		vr3->p_y -= 30;
 		img = mlx_xpm_file_to_image(vr3->init, mv.src, &mv.x_img, &mv.y_img);
 		mlx_put_image_to_window(vr3->init, vr3->wind, img, vr3->p_x, vr3->p_y);
-		img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
-		mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		if (vr3->td_map[mv.y / 30][mv.x / 30] != 'E')
+		{
+			img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
+			mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		}
 	}
 }
 
@@ -45,8 +48,11 @@ void	down(t_unit *vr3)
 		vr3->p_y += 30;
 		img = mlx_xpm_file_to_image(vr3->init, mv.src, &mv.x_img, &mv.y_img);
 		mlx_put_image_to_window(vr3->init, vr3->wind, img, vr3->p_x, vr3->p_y);
-		img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
-		mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		if (vr3->td_map[mv.y / 30][mv.x / 30] != 'E')
+		{
+			img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
+			mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		}
 	}
 }
 
@@ -64,8 +70,11 @@ void	right(t_unit *vr3)
 		vr3->p_x += 30;
 		img = mlx_xpm_file_to_image(vr3->init, mv.src, &mv.x_img, &mv.y_img);
 		mlx_put_image_to_window(vr3->init, vr3->wind, img, vr3->p_x, vr3->p_y);
-		img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
-		mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		if (vr3->td_map[mv.y / 30][mv.x / 30] != 'E')
+		{
+			img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
+			mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		}
 	}
 }
 
@@ -83,7 +92,10 @@ void	left(t_unit *vr3)
 		vr3->p_x -= 30;
 		img = mlx_xpm_file_to_image(vr3->init, mv.src, &mv.x_img, &mv.y_img);
 		mlx_put_image_to_window(vr3->init, vr3->wind, img, vr3->p_x, vr3->p_y);
-		img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
-		mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		if (vr3->td_map[mv.y / 30][mv.x / 30] != 'E')
+		{
+			img = mlx_xpm_file_to_image(vr3->init, mv.floor, &mv.x_img, &mv.y_img);
+			mlx_put_image_to_window(vr3->init, vr3->wind, img, mv.x, mv.y);
+		}
 	}
 }
