5c20fed177926aeef7561695584f14ac184c906d
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-12-19 11:48:35 +0100

    movements

--- my_lib/sl_utils/movement.c
+++ my_lib/sl_utils/movement.c
@@ -6,12 +6,28 @@
 /*   By: ahel-mou <ahel-mou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/18 14:02:20 by ahel-mou          #+#    #+#             */
-/*   Updated: 2021/12/18 16:43:02 by ahel-mou         ###   ########.fr       */
+/*   Updated: 2021/12/18 17:50:29 by ahel-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../full_libft.h"
 
+void check_C_or_E(int x, int y, t_unit *vars)
+{
+	static int i;
+
+	if (vars->td_map[y][x] == 'C')
+	{
+		vars->td_map[y][x] = '0';
+		i++;
+	}
+	if (i == vars->collect)
+		vars->unlock = 6969;
+	if (vars->td_map[y][x] == 'E' && i == vars->collect)
+	{
+		exit(1);
+	}
+}
 int	movement(int key, t_unit *vr2)
 {
 	if (key == 0)
@@ -22,5 +38,6 @@ int	movement(int key, t_unit *vr2)
 		up(vr2);
 	else if (key == 2)
 		right(vr2);
+	check_C_or_E(vr2->p_x / 30, vr2->p_y / 30, vr2);
 	return (1);
 }
