5bf7bb484c3f88d7d7ec99c9a457bef77db2b973
mountassir-007 <ahmedelmountassir10@gmail.com>
2021-12-22 10:37:31 +0100

    get next line

---
+++ bonus/my_lib/gnl/get_next_line.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahel-mou <ahel-mou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 13:56:24 by ahel-mou          #+#    #+#             */
+/*   Updated: 2021/12/21 18:05:59 by ahel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../bonus_libft.h"
+
+char	*extract_cur_line(char *stc_arr)
+{
+	int	i;
+
+	i = 0;
+	if (!stc_arr[i])
+		return (NULL);
+	i = 0;
+	while (stc_arr[i] && stc_arr[i] != '\n')
+		i++;
+	return (gnl_ft_substr(stc_arr, 0, i + 1));
+}
+
+char	*assign_rest(char *stc_arr, char *line)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!stc_arr)
+		return (NULL);
+	while (stc_arr[i])
+	{
+		if (stc_arr[i] == '\n')
+		{
+			str = gnl_ft_substr(stc_arr, i + 1, gnl_ft_strlen(stc_arr));
+			free (stc_arr);
+			if (str[0] == '\n' || str[0] == '\0' || (line[0] == '\n'))
+			{
+				free(line);
+				printf("new line in map\n");
+				exit(1);
+			}
+			return (str);
+		}
+		i++;
+	}
+	free (stc_arr);
+	return (NULL);
+}
+
+int	find_nl(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stc_arr[10240];
+	t_storage	unit_1;
+
+	if (fd < 0)
+		return (NULL);
+	unit_1.rd_len = 1;
+	unit_1.buff = malloc(100 + 1);
+	while (!(find_nl(stc_arr[fd])) && (unit_1.rd_len))
+	{
+		unit_1.rd_len = read(fd, unit_1.buff, 100);
+		if (unit_1.rd_len < 0)
+		{
+			free(unit_1.buff);
+			return (NULL);
+		}
+		unit_1.buff[unit_1.rd_len] = '\0';
+		stc_arr[fd] = gnl_ft_strjoin(stc_arr[fd], unit_1.buff);
+	}
+	free(unit_1.buff);
+	unit_1.cur_line = extract_cur_line(stc_arr[fd]);
+	stc_arr[fd] = assign_rest(stc_arr[fd], unit_1.cur_line);
+	return (unit_1.cur_line);
+}
---
+++ bonus/my_lib/gnl/get_next_line_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahel-mou <ahel-mou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/18 13:56:31 by ahel-mou          #+#    #+#             */
+/*   Updated: 2021/12/21 18:05:51 by ahel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../bonus_libft.h"
+
+int	gnl_ft_strlen(char *cara)
+{
+	int	i;
+
+	i = 0;
+	if (!cara)
+		return (i);
+	while (cara[i])
+		i++;
+	return (i);
+}
+
+char	*gnl_ft_strjoin(char *s1, char *s2)
+{
+	char	*new;
+	int		i;
+	int		j;
+	int		o_len;
+	int		d_len;
+
+	o_len = gnl_ft_strlen(s1);
+	d_len = gnl_ft_strlen(s2);
+	new = malloc((o_len + d_len + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < o_len)
+		new[i++] = s1[j++];
+	j = 0;
+	while (j < d_len)
+		new[i++] = s2[j++];
+	new[i] = '\0';
+	free(s1);
+	return (new);
+}
+
+char	*gnl_ft_substr(char *s, int start, int len)
+{
+	char	*cara;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (0);
+	if (start >= gnl_ft_strlen(s))
+		len = 0;
+	i = 0;
+	j = 0;
+	while (i < len && s[i])
+		i++;
+	cara = malloc((i + 1) * sizeof(char));
+	if (!cara)
+		return (NULL);
+	while (j < i)
+		cara[j++] = s[start++];
+	cara[i] = '\0';
+	return (cara);
+}
